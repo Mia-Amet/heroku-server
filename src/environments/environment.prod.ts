d04480fb3ac91638a439a40632230b665fc27a43
Mia-Amet <dashaags@gmail.com>
2021-05-09 04:41:02 +0300

    bug fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://jsonplaceholder.typicode.com',
+  firebase: {
+    apiKey: "AIzaSyC1MspdQgYc--9X06aMibnfrR99uxTzYXA",
+    authDomain: "auth-routing-1d131.firebaseapp.com",
+    projectId: "auth-routing-1d131",
+    storageBucket: "auth-routing-1d131.appspot.com",
+    messagingSenderId: "288298854830",
+    appId: "1:288298854830:web:5b5797044229f5d348003f",
+    measurementId: "G-64LK6NNGCY"
+  }
 };
